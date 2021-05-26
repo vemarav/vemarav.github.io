c12fdcd6f5cd1febbdee77c792697b01512eb2ad
vemarav <vemarav@outlook.in>
2021-05-26 23:11:56 +0530

    added panel

---
+++ src/images/index.js
@@ -0,0 +1,9 @@
+import bg from "../images/bg.svg";
+import detail from "../images/detail.svg";
+import flutter from "../images/flutter-tile.svg";
+import header from "../images/header.svg";
+import reactNative from "../images/react-native-tile.svg";
+import rubyOnRails from "../images/ruby-on-rails-tile.svg";
+
+const Svg = { bg, header, detail, flutter, reactNative, rubyOnRails };
+export default Svg;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import bgSvg from "../images/bg.svg";
+import svg from "../images";
 
 const Background = styled.div`
   height: 100vh;
-  background-image: url(${bgSvg});
+  background-image: url(${svg.bg});
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
@@ -29,12 +29,57 @@ const Glass = styled.div`
   display: flex;
 `;
 
-// const Panel = styled.div``;
+const Panel = styled.div`
+  position: relative;
+  width: 25%;
+  height: calc(100vh * 0.8);
+  left: 0px;
+  top: 0px;
+
+  background: linear-gradient(
+    107.56deg,
+    rgba(255, 255, 255, 0.48) 0%,
+    rgba(255, 255, 255, 0.16) 100%
+  );
+  backdrop-filter: blur(60px);
+  /* Note: backdrop-filter has minimal browser support */
+
+  border-radius: 30px 0px 0px 30px;
+`;
+
+const Header = styled.img`
+  margin: auto;
+  margin-top: 2rem;
+  height: 150px;
+  width: 150px;
+  display: block;
+`;
+
+const Tile = styled.img`
+  margin: 1.5rem auto;
+  width: 150px;
+  display: block;
+`;
+
+const Detail = styled.img`
+  margin: auto;
+  margin-top: 2rem;
+  width: 160px;
+  display: block;
+`;
 
 const Home = () => {
   return (
     <Background>
-      <Glass></Glass>
+      <Glass>
+        <Panel>
+          <Header src={svg.header} alt={"Aravind Vemula"} />
+          <Tile src={svg.flutter} alt="Flutter" />
+          <Tile src={svg.reactNative} alt="React Native" />
+          <Tile src={svg.rubyOnRails} alt="Ruby on Rails" />
+          <Detail src={svg.detail} alt="aravind-detail" />
+        </Panel>
+      </Glass>
     </Background>
   );
 };
